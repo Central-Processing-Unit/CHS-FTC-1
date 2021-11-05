c577954b8b21d8f9c08f21f79abaf0a869006727
danielhuinda <huindam@gmail.com>
2021-11-05 11:29:48 -0500

    Added initVuforia function for localization and tracking.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonLogic.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonLogic.java
@@ -1,7 +1,24 @@
 package org.firstinspires.ftc.teamcode;
 
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+
 public class AutonLogic extends Base
 {
+    private WebcamName webcameName;
+    private VuforiaLocalizer vuforiaLocalizer;
+
+    public void InitVuforia()
+    {
+        VuforiaLocalizer.Parameters params = new VuforiaLocalizer.Parameters();
+
+        params.vuforiaLicenseKey = "AS0ENI3/////AAABmRrhaZtkGkSMi4tGQFf9azI3tZlg7Xv8GCAFy/EtV7oDQmsVBBNgiQNq035C7ShFgSt1Y9dtgOUrPHhlgoI/8sqhoBUnr3WRm/ex/gPsScPYlpy4mqBUZEIQxI2hndIuFrxPSc5gCMC4kyay2RWUWthzUygnp/22kgrq2u7xyKLwsUIctziWB1T3xreY6LcdSuqgPx6qMeiOmPkqLrIm+BbJovtmoVA7d/PqPoIeoo6O/CurFZVUeJq7zkPRB9OzsoF3Iyxyd3jGi1xlPes828QsbIcx1UYQIyR+q52fLVAt69FPPQ6AO8YMfgc0z+qF7pSA1Vee1LIyF+HCMh67gXj3YntVhvlnSeflrFtVB7vl";
+        params.cameraName = hardwareMap.get(WebcamName.class, "MainCamera");
+
+        vuforiaLocalizer = ClassFactory.getInstance().createVuforia(params);
+    }
+
     public void go(double xtarget, double ytarget)
     {
 
